9a160a994c6c711667e081c582862033ba9766b0
Austin Condiff <austin.condiff@gmail.com>
2018-05-07 02:27:00 -0500

    Form makes post request to formcarry

--- src/components/Blocks/Connect.js
+++ src/components/Blocks/Connect.js
@@ -5,11 +5,8 @@ export default class Connect extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      formData: {
-        Field16: '',
-        Field3: '',
-        Field6: ''
-      }
+      formData: {},
+      submitted: false
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -27,31 +24,15 @@ export default class Connect extends Component {
 
   handleSubmit(e) {
     e.preventDefault()
-
-    const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    const url = "https://creator.zoho.com/api/acondiff/json/portfolio/form/Contact/record/add";
+    const url = "https://formcarry.com/s/HkACBO6Tz";
 
     axios({
-      url: url,
       method: "POST",
-      async: true,
-      crossDomain: true,
-      mimeType: "multipart/form-data",
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-        "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-      },
-      data: {
-        "authtoken": "f513e7ba9a5b14f3ac8d3b12382559d9",
-        "scope": "creatorapi",
-        "Name.first_name": this.state.formData.firstName,
-        "Name.last_name": this.state.formData.lastName,
-        "Email": this.state.formData.email,
-        "Message": this.state.formData.message
-      }
+      dataType: "json",
+      url: url,
+      data: this.state.formData,
     }).then(data => {
-      console.log(data)
+      this.setState({submitted: true})
     }).catch(data => {
       console.log('There was a problem submitting the form.')
       console.log(data)
@@ -59,57 +40,69 @@ export default class Connect extends Component {
   }
 
   render() {
-    const { formData } = this.state
+    const { formData, submitted } = this.state
     return (
       <div className="block connect" id="connect">
     		<div className="wrap">
-    			<h3 className="title">Connect with Me</h3>
-    			<p className="connect-txt">Like my work? Want to know more about me? Like to share new ideas? Want to meet up? Feel free to connect with me!</p>
-    			<form id="form1" name="form1" className="contact wufoo topLabel page" onSubmit={this.handleSubmit}>
-    				<ul>
-            <li id="field-firstName" className="notranslate first-name">
-              <label className="desc" id="title16" htmlFor="firstName">
-                First Name
-                <span id="req_16" className="req">*</span>
-              </label>
-              <div>
-                <input id="firstName" name="firstName" type="text" className="field text medium name" value={formData.name} onChange={this.handleChange} maxLength="255" tabIndex="1" required placeholder="First Name" />
-              </div>
-            </li>
-            <li id="field-lastName" className="notranslate last-name">
-              <label className="desc" id="title16" htmlFor="lastName">
-                Last Name
-                <span id="req_16" className="req">*</span>
-              </label>
-              <div>
-                <input id="lastName" name="lastName" type="text" className="field text medium name" value={formData.name} onChange={this.handleChange} maxLength="255" tabIndex="1" required placeholder="Last Name" />
-              </div>
-            </li>
-    					<li id="foli3" className="notranslate email">
-    						<label className="desc" id="title3" htmlFor="email">
-    							Email
-    							<span id="req_3" className="req">*</span>
-    						</label>
-    						<div>
-    							<input id="email" name="email" type="email" spellCheck="false" className="field text large email" value={formData.email} onChange={this.handleChange} maxLength="255" tabIndex="2" required placeholder="Email"/>
-    						</div>
-    					</li>
-    					<li id="foli6" className="notranslate message">
-    						<label className="desc" id="title6" htmlFor="message">
-    							Message
-    							<span id="req_6" className="req">*</span>
-    						</label>
-    						<div>
-    							<textarea id="message" name="message" className="field textarea large message" spellCheck="true" value={formData.message} onChange={this.handleChange} rows="10" cols="50" tabIndex="3" required placeholder="Message"></textarea>
-    						</div>
-    					</li>
-    					<li className="buttons submit">
-    						<div>
-    							<input id="saveForm" name="saveForm" className="btTxt submit" type="submit" value="Submit"/>
-    						</div>
-    					</li>
-    				</ul>
-    			</form>
+          <h3 className="title">Connect with Me</h3>
+          {submitted ? (
+            <p className="connect-txt">Got it! I'll get in touch as soon as I can.</p>
+          ) : (
+            <form className="contact page" onSubmit={this.handleSubmit}>
+        			<p className="connect-txt">Like my work? Want to know more about me? Like to share new ideas? Want to meet up? Feel free to connect with me!</p>
+              <ul>
+                <li className="name">
+                  <input
+                    id="name"
+                    name="Name"
+                    type="text"
+                    className="field text name"
+                    value={formData.name}
+                    onChange={this.handleChange}
+                    maxLength="255"
+                    tabIndex="1"
+                    required
+                    placeholder="Name" />
+                </li>
+      					<li className="email">
+    							<input
+                    id="email"
+                    name="Email"
+                    type="email"
+                    spellCheck="false"
+                    className="field text email"
+                    value={formData.email}
+                    onChange={this.handleChange}
+                    maxLength="255"
+                    tabIndex="2"
+                    required
+                    placeholder="Email"/>
+      					</li>
+      					<li className="message">
+    							<textarea
+                    id="message"
+                    name="Message"
+                    className="field textarea message"
+                    spellCheck="true"
+                    value={formData.message}
+                    onChange={this.handleChange}
+                    rows="10"
+                    cols="50"
+                    tabIndex="3"
+                    required
+                    placeholder="Message"></textarea>
+      					</li>
+      					<li className="buttons submit">
+    							<input
+                    id="saveForm"
+                    name="saveForm"
+                    className="btTxt submit"
+                    type="submit"
+                    value="Submit"/>
+      					</li>
+      				</ul>
+      			</form>
+          )}
     		</div>
     	</div>
     )
