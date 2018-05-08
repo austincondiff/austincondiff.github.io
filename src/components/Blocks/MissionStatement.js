15c2b794434b7b3142279ec975936b479e2ec6f4
Austin Condiff <austin.condiff@gmail.com>
2018-05-08 12:41:56 -0500

    Modified mission statement

--- src/components/Blocks/MissionStatement.js
+++ src/components/Blocks/MissionStatement.js
@@ -5,7 +5,9 @@ export default class MissionStatement extends Component {
     return (
       <div className="block mission-statement">
     		<div className="wrap">
-    			<h3>I design beautiful and simple user interfaces, brand identities, and interactive user experiences.</h3>
+          <h3>I craft and deliver beautiful and simple user interfaces, brand identities, and interactive user experiences. </h3>
+          <p>For the past fifteen years I have been designing and developing interactive user experiences using the best design techniques and the latest front-end technologies to ensure quality work is delivered.</p>
+          <p>What started as a favor at the age of fourteen to create a simple website for my parents’ small photography business later turned into a passion and life-long dream. What has gone from a skill or career path has grown into a passion.</p>
     		</div>
     	</div>
     )
