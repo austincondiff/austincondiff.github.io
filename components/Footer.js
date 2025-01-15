d2ac3516474eaefb0a5094295b27e41227b05e5a
Austin Condiff <austin.condiff@gmail.com>
2025-01-15 02:10:30 -0600

    Fixed ESLint issue

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ export default class Footer extends Component {
     return (
       <footer className="block">
     		<div className="wrap center-text">
-          <p>&copy; {new Date().getFullYear()} Austin Condiff. Like my website? Feel free to <a href="https://github.com/austincondiff/austincondiff.github.io" target="_blank">fork it</a> on GitHub!</p>
+          <p>&copy; {new Date().getFullYear()} Austin Condiff. Like my website? Feel free to <a href="https://github.com/austincondiff/austincondiff.github.io" target="_blank" rel="noreferrer">fork it</a> on GitHub!</p>
         </div>
     	</footer>
     )
