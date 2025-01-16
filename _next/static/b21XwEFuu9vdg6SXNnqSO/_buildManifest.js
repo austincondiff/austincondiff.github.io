7842030431414206024f1c6498fbb2d5fa827a60
austincondiff <austin.condiff@gmail.com>
2025-01-16 19:05:18 +0000

    Deploying to gh-pages from @ austincondiff/austincondiff.github.io@e349578b096658afd31d0b0f1844917c17d5d421 🚀

---
+++ _next/static/b21XwEFuu9vdg6SXNnqSO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/75fc9c18-8a887c49b3994da9.js","static/chunks/682-9e08d6685d7fd9a8.js","static/chunks/pages/index-70970d6dd626afbd.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
